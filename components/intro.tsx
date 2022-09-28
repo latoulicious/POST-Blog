0692ac771b6b97fbf4b10ee921a94bcba652fb22
latoulicious <count.kramm48@gmail.com>
2022-09-28 22:01:07 +0700

    ✨

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,10 +3,10 @@ import { CMS_NAME } from '../lib/constants'
 const Intro = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-2  mb-16  md:mb-6">
-      <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+      <h1 className="text-3xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+        Dev.
       </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
+      <h4 className="text-center md:text-left text-lg mt-6 md:pl-8">
         {CMS_NAME}  Discord Community Blog.
       </h4>
     </section>
