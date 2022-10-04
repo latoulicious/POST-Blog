88fcf8d532a631d49337d72766dd70d6ef96aa67
latoulicious <count.kramm48@gmail.com>
2022-10-04 20:32:14 +0700

    🚀

--- components/footer.tsx
+++ components/footer.tsx
@@ -11,17 +11,14 @@ const Footer = () => {
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="https://discord.com/invite/BfdSS4kfjM"
+              href="https://github.com/latoulicious/POSTBlog"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
-              Invite Link
+              Github
             </a>
-            <a
-              href={`https://github.com/latoulicious/POSTBlog`}
-              className="mx-3 font-bold hover:underline"
-            >
-              GitHub
-            </a>
+            <iframe src="https://discord.com/widget?id=605016448938803210&theme=dark" width="512" height="500" allowtransparency="true" frameborder="20"
+            sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts">
+            </iframe>
           </div>
         </div>
       </Container>
